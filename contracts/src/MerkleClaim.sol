c675a1878a31626508195413523c8062a01bb7a7
Schalk Dormehl <schalk.dormehl@gmail.com>
2023-01-08 16:07:14 +0200

    Claim via "tranferFrom"

---
+++ contracts/src/MerkleClaim.sol
@@ -0,0 +1,84 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity >=0.8.0;
+
+/// ============ Imports ============
+
+import { IERC20 } from "@openzeppelin/token/ERC20/IERC20.sol"; // OZ: IERC20
+import { MerkleProof } from "@openzeppelin/utils/cryptography/MerkleProof.sol"; // OZ: MerkleProof
+
+/// @title MerkleClaim
+/// @notice Tokens claimable by members of a merkle tree
+/// @author Anish Agnihotri <contact@anishagnihotri.com>
+contract MerkleClaim {
+
+  /// ============ Immutable storage ============
+
+  /// @notice claimee inclusion root
+  bytes32 public immutable merkleRoot;
+
+  /// @notice ERC20 token
+  IERC20 public immutable token;
+
+  /// @notice Source of funds address
+  address public immutable fundSource;
+
+  /// ============ Mutable storage ============
+
+  /// @notice Mapping of addresses who have claimed tokens
+  mapping(address => bool) public hasClaimed;
+
+  /// ============ Errors ============
+
+  /// @notice Thrown if address has already claimed
+  error AlreadyClaimed();
+  /// @notice Thrown if address/amount are not part of Merkle tree
+  error NotInMerkle();
+
+  /// ============ Constructor ============
+
+  /// @notice Creates a new MerkleClaimERC20 contract
+  /// @param _token of token to be claimed
+  /// @param _fundSource of account containing disbursement funds
+  /// @param _merkleRoot of claimees
+  constructor(
+    address _token,
+    address _fundSource,
+    bytes32 _merkleRoot
+  )  {
+    token = IERC20(_token); // Update token
+    fundSource = _fundSource; // Update fund source
+    merkleRoot = _merkleRoot; // Update root
+  }
+
+  /// ============ Events ============
+
+  /// @notice Emitted after a successful token claim
+  /// @param to recipient of claim
+  /// @param amount of tokens claimed
+  event Claim(address indexed to, uint256 amount);
+
+  /// ============ Functions ============
+
+  /// @notice Allows claiming tokens if address is part of merkle tree
+  /// @param to address of claimee
+  /// @param amount of tokens owed to claimee
+  /// @param proof merkle proof to prove address and amount are in tree
+  function claim(address to, uint256 amount, bytes32[] calldata proof) external {
+    // Throw if address has already claimed tokens
+    if (hasClaimed[to]) revert AlreadyClaimed();
+
+    // Verify merkle proof, or revert if not in tree
+    bytes32 leaf = keccak256(abi.encodePacked(to, amount));
+    bool isValidLeaf = MerkleProof.verify(proof, merkleRoot, leaf);
+    if (!isValidLeaf) revert NotInMerkle();
+
+    // Set address to claimed
+    hasClaimed[to] = true;
+
+    // Send tokens to cliamee from fund source
+    token.transferFrom(fundSource, to, amount);
+
+    // Emit claim event
+    emit Claim(to, amount);
+  }
+}
--- frontend/config.ts
+++ frontend/config.ts
@@ -4,14 +4,31 @@ type IConfig = {
   airdrop: Record<string, number>;
 };
 
+const airdrop = {
+    "decimals": "18",
+    "airdrop": {
+      "0x016C8780e5ccB32E5CAA342a926794cE64d9C364": "10",
+      "0x185a4dc360ce69bdccee33b3784b0282f7961aea": "100",
+    },
+  };
+
 // Config from generator
-const config: IConfig = {
-  decimals: 18,
-  airdrop: {
-    "0x016C8780e5ccB32E5CAA342a926794cE64d9C364": 10,
-    "0x185a4dc360ce69bdccee33b3784b0282f7961aea": 100,
-  },
-};
+// const config: IConfig = {
+//   decimals: 18,
+//   "airdrop": {
+//     "0x016C8780e5ccB32E5CAA342a926794cE64d9C364": 10,
+//     "0x185a4dc360ce69bdccee33b3784b0282f7961aea": 100,
+//   },
+// };
+
+function convertJsonToIConfig(json: any): IConfig {
+  return {
+    decimals: Number(json.decimals),
+    airdrop: json.airdrop
+  };
+}
+
+const config = convertJsonToIConfig(airdrop);
 
 // Export config
 export default config;
